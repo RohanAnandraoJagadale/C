339eb08f75d260dd7714a0c0979a9bfad736b796
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-11-01 14:59:13 +0530

    Add files via upload

---
+++ iteration22.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int iCnt = 0;
+	
+	 for (iCnt=1; iCnt<109; iCnt++)
+	 {
+	   printf("Om Namah Shivay..\n");
+	 }
+
+
+      return 0;
+}
---
+++ iteration66.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+     int iCnt = 0;
+	  iCnt = 1;
+	 
+	 do
+	 {
+	    printf("Om Namah Shivay..\n");
+		iCnt++;
+	 
+	 }while(iCnt<109);
+
+
+
+     return 0;
+}
